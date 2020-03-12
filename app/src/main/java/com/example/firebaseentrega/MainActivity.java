c18d7631dd2890331849b666ace50db9e1b11999
sbravor64 <Andresbravo97>
2020-03-12 15:30:07 +0100

    qq

--- app/src/main/java/com/example/firebaseentrega/MainActivity.java
+++ app/src/main/java/com/example/firebaseentrega/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+//test(Jiayu,) para subir.
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
